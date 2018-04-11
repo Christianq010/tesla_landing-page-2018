b5f40821cd60c1bcd6251928418186ebf486deec
Christiaan Quyn <christianq010@gmail.com>
2018-04-11 21:06:20 +0530

    Add NavBar Component

---
+++ src/components/Nav/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
+    <div class="container-2 w-container">
+      <div class="small-nav-menu">
+        <a href="#" class="small-nav-item">Shop</a>
+        <a href="#" class="small-nav-item">Sign in</a>
+      </div>
+      <!-- Our Logo -->
+      <a href="index.html" class="logo w-nav-brand">
+        <img src="./../../assets/tesla-white.png" alt="Tesla Logo" sizes="7vw" class="image">
+      </a>
+      <nav role="navigation" class="nav-menu w-nav-menu">
+        <a href="#" class="nav-item w-nav-link">Model S</a>
+        <a href="#" class="nav-item w-nav-link">Model X</a>
+        <a href="#" class="nav-item w-nav-link">Model 3</a>
+        <a href="#" class="nav-item w-nav-link">Roadster</a>
+        <a href="#" class="nav-item w-nav-link">energy</a>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+// import Tesla Logo
+import teslaLogo from './../../assets/logo.png'
+
+export default {
+
+}
+
+
+</script>
+
+<style>
+@import './../../assets/CSS/normalize.css';
+@import './../../assets/CSS/webflow.css';
+@import './../../assets/CSS/custom.css';
+
+</style>
