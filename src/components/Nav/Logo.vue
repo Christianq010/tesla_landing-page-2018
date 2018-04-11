bd3ce8fb75fc554a6890fc6756a56fa81aa82d74
Christiaan Quyn <christianq010@gmail.com>
2018-04-11 21:18:00 +0530

    refactor: NavBar - logo into its own component, add hover styles

---
+++ src/components/Nav/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+      <!-- Our Logo -->
+      <a href="index.html" class="logo w-nav-brand">
+        <img src="./../../assets/tesla-white.png" alt="Tesla Logo" sizes="7vw" class="image">
+      </a>
+</template>
+
+<script>
+// import Tesla Logo
+import teslaLogo from './../../assets/logo.png'
+
+export default {
+
+}
+
+
+</script>
+
+<style>
+@import './../../assets/CSS/normalize.css';
+@import './../../assets/CSS/webflow.css';
+@import './../../assets/CSS/custom.css';
+
+</style>
--- src/components/Nav/NavBar.vue
+++ src/components/Nav/NavBar.vue
@@ -1,14 +1,14 @@
 <template>
   <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
     <div class="container-2 w-container">
+      <!-- Nav Item List right -->
       <div class="small-nav-menu">
         <a href="#" class="small-nav-item">Shop</a>
         <a href="#" class="small-nav-item">Sign in</a>
       </div>
-      <!-- Our Logo -->
-      <a href="index.html" class="logo w-nav-brand">
-        <img src="./../../assets/tesla-white.png" alt="Tesla Logo" sizes="7vw" class="image">
-      </a>
+      <!-- Tesla Logo -->
+      <logo />
+      <!-- Nav Items center -->
       <nav role="navigation" class="nav-menu w-nav-menu">
         <a href="#" class="nav-item w-nav-link">Model S</a>
         <a href="#" class="nav-item w-nav-link">Model X</a>
@@ -22,10 +22,12 @@
 
 <script>
 // import Tesla Logo
-import teslaLogo from './../../assets/logo.png'
+import teslaLogo from './Logo.vue'
 
 export default {
-
+    components: {
+        logo: teslaLogo
+    }
 }
 
 
@@ -35,5 +37,9 @@ export default {
 @import './../../assets/CSS/normalize.css';
 @import './../../assets/CSS/webflow.css';
 @import './../../assets/CSS/custom.css';
+
+a:hover {
+    color: #666;
+}
 
 </style>
