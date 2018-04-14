fd683fac5f3f76a850f399e955f20f344cbf2880
Christiaan Quyn <christianq010@gmail.com>
2018-04-14 15:46:27 +0530

    Feat: Add Mask span Component Creates a layer over all content except NavBar when Menu icon clicked.

---
+++ src/components/Mask.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- This Mask covers all content except SideBav when Menu Icon is Clicked -->
+    <span id="mask"></span>
+</template>
+
+<script>
+    export default {
+    
+    }
+</script>
+
+<style>
+
+    /* BG Cover */
+    /* Cover other screen content when Menu is clicked */
+    #mask {
+        top: 0;
+        right: 200px;
+        height: 100vh;
+        width: 100%;
+        position: fixed;
+        overflow: hidden;
+        z-index: 20;
+        opacity: 1;
+        cursor: default;
+        background-color: rgba(34,34,34,0.25);
+        transition: opacity 300ms ease-in-out;
+    }
+
+</style>
